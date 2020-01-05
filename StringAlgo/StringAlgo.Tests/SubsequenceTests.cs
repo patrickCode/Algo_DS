c44fdebda0a1e6b3725a92e1c6c0d9e64d369de7
Pratik Bhattacharya <patrickb404@outlook.com>
2020-01-05 06:14:36 +0530

    Subsequence

---
+++ StringAlgo/StringAlgo.Tests/SubsequenceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringAlgo.Tests
+{
+    [TestClass]
+    public class SubsequenceTests
+    {
+        [TestMethod]
+        public void abc_is_subsequence_of_abhdc()
+        {
+            var source = "abc";
+            var target = "abhdc";
+            Assert.IsTrue(Subsequence.IsSubsequence(source, target));
+        }
+
+        [TestMethod]
+        public void axc_is_not_subsequence_of_abhdc()
+        {
+            var source = "axc";
+            var target = "abhdc";
+            Assert.IsFalse(Subsequence.IsSubsequence(source, target));
+        }
+    }
+}
---
+++ StringAlgo/StringAlgo/Subsequence.cs
@@ -0,0 +1,28 @@
+﻿namespace StringAlgo
+{
+    /*
+     * LeetCode
+     * https://www.youtube.com/watch?v=UulHAjxjZ4o
+     * Find if a string is a substring of another string
+     * Time - O(n)
+     * Space - O(n)
+     */
+    public class Subsequence
+    {
+        public static bool IsSubsequence(string source, string target)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                return true;
+
+            var sourceIndex = 0;
+            for(var targetIndex = 0; targetIndex < target.Length; targetIndex++)
+            {
+                if (source[sourceIndex] == target[targetIndex])
+                    sourceIndex++;
+                if (sourceIndex == source.Length)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
