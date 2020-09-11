44dfad78e1806c7d08ebf847d6267e3d747257b1
Pratik Bhattacharya <patrickb404@outlook.com>
2020-09-12 00:29:51 +0530

    Largest square in a matrix of 1 and 0

---
+++ DynProg/DynProg/LargestSquareMatrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DynProg
+{
+    public static class LargestSquareMatrix
+    {
+        public static int GetLargestSquareMatrix(int[,] matrix)
+        {
+            int length = (int)Math.Sqrt(matrix.Length);
+            int[,] cache = new int[length + 1, length + 1];
+            var globalMaxSquare = 0;
+            for (int row = 0; row < length + 1; row++)
+            {
+                for (int col = 0; col < length + 1; col++)
+                {
+                    if (row == 0 || col == 0)
+                    {
+                        cache[row, col] = 0;
+                        continue;
+                    }
+
+                    int cell = matrix[row - 1, col - 1];
+                    int localMaxSquare = cell == 1
+                        ? Math.Min(Math.Min(cache[row - 1, col - 1], cache[row - 1, col]), cache[row, col - 1]) + 1
+                        : 0;
+                    cache[row, col] = localMaxSquare;
+                    globalMaxSquare = localMaxSquare > globalMaxSquare ? localMaxSquare : globalMaxSquare;
+                }
+            }
+            return globalMaxSquare;
+        }
+
+        public static void Test()
+        {
+            int[,] matrix = new int[,]
+            {
+                { 0, 1, 0, 1, 1 },
+                { 0, 1, 1, 1, 1 },
+                { 0, 0, 1, 1, 1 },
+                { 0, 0, 1, 1, 1 },
+                { 0, 1, 1, 1, 1 }
+            };
+
+            //int[,] matrix = new int[,]
+            //{
+            //    { 1, 1, 1, 1, 1 },
+            //    { 1, 1, 1, 1, 1 },
+            //    { 1, 1, 1, 1, 1 },
+            //    { 1, 1, 1, 1, 1 },
+            //    { 1, 1, 1, 1, 1 }
+            //};
+            int maxSquare = GetLargestSquareMatrix(matrix);
+            Console.WriteLine($"Max Square: {maxSquare}");
+        }
+    }
+}
---
+++ DynProg/DynProg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DynProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LargestSquareMatrix.Test();
+            Console.ReadKey();
+        }
+    }
+}
