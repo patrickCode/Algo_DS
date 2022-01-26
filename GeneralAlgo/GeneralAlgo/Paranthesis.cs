db1ecdeac0d1c49706dd05c9ee091cc9ca3ace6f
Pratik Bhattacharya <pratikb@microsoft.com>
2022-01-26 16:49:06 +0530

    algo: added valid paranthesis algo: added paranthesis permutation

---
+++ GeneralAlgo/GeneralAlgo/Paranthesis.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralAlgo
+{
+    public static class Paranthesis
+    {
+        public static List<string> Generated = new();
+
+        public static bool IsValid(string paranthesis)
+        {
+            int paranthesisCounter = 0;
+
+            foreach(char p in paranthesis)
+            {
+                if (p == '(')
+                    paranthesisCounter++;
+                else
+                    paranthesisCounter--;
+
+                if (paranthesisCounter < 0)
+                    return false;
+            }
+
+            return paranthesisCounter == 0;
+        }
+
+        public static List<string> Generate(int n)
+        {
+            Generated.Clear();
+            Generate("", '(', n, 1, 0);
+            return Generated;
+        }
+
+        private static void Generate(string generatedString, char paranthesis, int n, int openParanthesis, int closedParanthesis)
+        {
+            if (closedParanthesis > openParanthesis)
+                return;
+
+            if (openParanthesis + closedParanthesis == (n *2))
+            {
+                if (openParanthesis != closedParanthesis)
+                    return;
+
+                generatedString += paranthesis;
+                Generated.Add(generatedString.ToString());
+                return;
+            }
+
+            generatedString += paranthesis;
+            Generate(generatedString, '(', n, openParanthesis + 1, closedParanthesis);
+            Generate(generatedString, ')', n, openParanthesis, closedParanthesis + 1);
+        }
+    }
+}
---
+++ GeneralAlgo/GeneralAlgoTests/ParanthesisTests.cs
@@ -0,0 +1,74 @@
+﻿using GeneralAlgo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneralAlgoTests
+{
+    [TestClass]
+    public class ParanthesisTests
+    {
+        [TestMethod]
+        public void TestCase_1()
+        {
+            string paranthesis = "((()))";
+            Assert.IsTrue(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void TestCase_2()
+        {
+            string paranthesis = "()()()";
+            Assert.IsTrue(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void TestCase_3()
+        {
+            string paranthesis = "(()())";
+            Assert.IsTrue(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void TestCase_4_Unclosed()
+        {
+            string paranthesis = "(()()";
+            Assert.IsFalse(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void TestCase_4_ExtraClosed()
+        {
+            string paranthesis = "(()()))";
+            Assert.IsFalse(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void TestCase_4_WrongClosed()
+        {
+            string paranthesis = "(()))()";
+            Assert.IsFalse(Paranthesis.IsValid(paranthesis));
+        }
+
+        [TestMethod]
+        public void Generate_1() 
+        {
+            List<string> paranthesis = Paranthesis.Generate(1);
+            Assert.IsTrue(paranthesis.Any());
+            Assert.AreEqual("()", paranthesis.First());
+        }
+
+
+        [TestMethod]
+        public void Generate_3()
+        {
+            List<string> paranthesis = Paranthesis.Generate(3);
+            Assert.AreEqual(5, paranthesis.Count());
+            Assert.IsTrue(paranthesis.Contains("((()))"));
+            Assert.IsTrue(paranthesis.Contains("(()())"));
+            Assert.IsTrue(paranthesis.Contains("(())()"));
+            Assert.IsTrue(paranthesis.Contains("()(())"));
+            Assert.IsTrue(paranthesis.Contains("()()()"));
+        }
+    }
+}
