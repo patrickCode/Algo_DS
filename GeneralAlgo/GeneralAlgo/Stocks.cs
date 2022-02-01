66ac39cca998c50c7de18bfe5e0d611667372a2a
Pratik Bhattacharya <pratikb@microsoft.com>
2022-02-02 03:36:07 +0530

    algo: max profit from stocks

---
+++ GeneralAlgo/GeneralAlgo/Stocks.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace GeneralAlgo
+{
+    public class Stocks
+    {
+        private readonly int[] _prices;
+
+        public Stocks(int[] prices)
+        {
+            _prices = prices;
+        }
+
+        public int GetMaxProfitWithSingleTransaction()
+        {
+            int buyPointer = 0;
+            int sellPointer = 1;
+
+            int maxProfit = -10000;
+
+            while (sellPointer < _prices.Count())
+            {
+                int costPrice = _prices[buyPointer];
+                int sellingPrice = _prices[sellPointer];
+
+                int profit = sellingPrice - costPrice;
+                if (profit > maxProfit)
+                    maxProfit = profit;
+
+                if (profit < 0)
+                {
+                    buyPointer = sellPointer;
+                }
+                sellPointer++;
+            }
+            return maxProfit;
+        }
+
+        public int GetMaxProfitWithMultipleTransaction()
+        {
+            int buyPointer = 0;
+            int sellPointer = 1;
+            int totalProfit = 0;
+
+            while (sellPointer < _prices.Count())
+            {
+                int costPrice = _prices[buyPointer];
+                int sellingPrice = _prices[sellPointer];
+
+                if (sellingPrice < costPrice)
+                {
+                    buyPointer = sellPointer;
+                    sellPointer++;
+                    continue;
+                }
+
+                int lastSellingPrice = sellingPrice;
+                while (sellingPrice >= lastSellingPrice)
+                {
+                    sellPointer++;
+                    sellingPrice = _prices[sellPointer];
+                }
+                totalProfit += (lastSellingPrice - costPrice);
+                buyPointer = sellPointer;
+                sellPointer++;
+            }
+            return totalProfit;
+        }
+    }
+}
---
+++ GeneralAlgo/GeneralAlgoTests/StocksTests.cs
@@ -0,0 +1,25 @@
+﻿using GeneralAlgo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeneralAlgoTests
+{
+    [TestClass]
+    public class StocksTests
+    {
+        [TestMethod]
+        public void TestCase1_MaxProfit_SingleTransaction()
+        {
+            Stocks stocks = new(new int[] { 7, 1, 5, 3, 6, 4 });
+            int maxProfit = stocks.GetMaxProfitWithSingleTransaction();
+            Assert.AreEqual(5, maxProfit);
+        }
+
+        [TestMethod]
+        public void TestCase1_MaxProfit_MultipleTransaction()
+        {
+            Stocks stocks = new(new int[] { 7, 1, 5, 3, 6, 4 });
+            int maxProfit = stocks.GetMaxProfitWithMultipleTransaction();
+            Assert.AreEqual(7, maxProfit);
+        }
+    }
+}
