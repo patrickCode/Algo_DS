b6fe5e7f7c8e5f6d39463324b7f398a9fc983e45
Pratik Bhattacharya <pratikb@microsoft.com>
2022-02-02 14:18:43 +0530

    algo: overlapping interval removal

---
+++ GeneralAlgo/GeneralAlgo/IntervalManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+// Reference: https://www.youtube.com/watch?v=nONCGxWoUfM
+
+namespace GeneralAlgo
+{
+    public class IntervalManager
+    {
+        private readonly List<Interval> _intervals;
+
+        public IntervalManager(List<Interval> intervals)
+        {
+            _intervals = intervals.OrderBy(interval => interval.StartTime).ToList();
+        }
+
+        public List<Interval> RemoveMinOverlappingIntervals()
+        {
+            int intervalIndex = 0;
+            List<Interval> removedIntervals = new();
+            while (intervalIndex < _intervals.Count - 1)
+            {
+                Interval currentInterval = _intervals[intervalIndex];
+                Interval adjacentInterval = _intervals[intervalIndex + 1];
+
+                if (currentInterval.IsOverlapping(adjacentInterval))
+                {
+                    Interval longerInterval = currentInterval.EndTime > adjacentInterval.EndTime
+                        ? currentInterval
+                        : adjacentInterval;
+                    _intervals.Remove(longerInterval);
+                    removedIntervals.Add(longerInterval);
+                    continue;
+                }
+                intervalIndex++;
+            }
+            return removedIntervals;
+        }
+    }
+
+    public class Interval
+    {
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+
+        public Interval(int startTime, int endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public bool IsOverlapping(Interval nextInterval)
+        {
+            return nextInterval.StartTime < EndTime;
+        }
+    }
+}
---
+++ GeneralAlgo/GeneralAlgoTests/IntervalManagerTests.cs
@@ -0,0 +1,48 @@
+﻿using GeneralAlgo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneralAlgoTests
+{
+    [TestClass]
+    public class IntervalManagerTests
+    {
+        [TestMethod]
+        public void TestCase1_RemoveSingleInterval()
+        {
+            List<Interval> intervals = new()
+            {
+                new Interval(1, 2),
+                new Interval(2, 3),
+                new Interval(3, 3),
+                new Interval(1, 3),
+            };
+            IntervalManager manager = new(intervals);
+            List<Interval> removedIntervals = manager.RemoveMinOverlappingIntervals();
+
+            Assert.AreEqual(1, removedIntervals.Count);
+            Assert.AreEqual(1, removedIntervals.First().StartTime);
+            Assert.AreEqual(3, removedIntervals.First().EndTime);
+        }
+
+        [TestMethod]
+        public void TestCase1_Remove2Intervals()
+        {
+            List<Interval> intervals = new()
+            {
+                new Interval(1, 2),
+                new Interval(1, 2),
+                new Interval(1, 2)
+            };
+            IntervalManager manager = new(intervals);
+            List<Interval> removedIntervals = manager.RemoveMinOverlappingIntervals();
+
+            Assert.AreEqual(2, removedIntervals.Count);
+            Assert.AreEqual(1, removedIntervals.First().StartTime);
+            Assert.AreEqual(2, removedIntervals.First().EndTime);
+            Assert.AreEqual(1, removedIntervals.Last().StartTime);
+            Assert.AreEqual(2, removedIntervals.Last().EndTime);
+        }
+    }
+}
