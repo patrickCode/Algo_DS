2d216bd7f6fc70fe09b4e7bccfc92bd659829cfb
Pratik Bhattacharya <pratikb@microsoft.com>
2022-02-04 02:33:42 +0530

    ll: reverse list from start to end point

---
+++ LinkLists/LinkLists.Tests/ReverseTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkLists.Tests
+{
+    [TestClass]
+    public class ReverseTests
+    {
+        [TestMethod]
+        public void TestCase1_ReverseList()
+        {
+            Node root = new(1);
+            root.next = new(2);
+            root.next.next = new(3);
+            root.next.next.next = new(4);
+            root.next.next.next.next = new(5);
+
+            Reverse rev = new();
+            rev.ReverseList(root, 2, 4);
+
+            Assert.AreEqual(1, root.data);
+            Assert.AreEqual(4, root.next.data);
+            Assert.AreEqual(3, root.next.next.data);
+            Assert.AreEqual(2, root.next.next.next.data);
+            Assert.AreEqual(5, root.next.next.next.next.data);
+        }
+    }
+}
---
+++ LinkLists/LinkLists/Reverse.cs
@@ -0,0 +1,50 @@
+﻿namespace LinkLists
+{   
+    public class Reverse
+    {
+        // Reference: https://www.youtube.com/watch?v=RF_M9tX4Eag
+        public void ReverseList(Node root, int start, int end)
+        {
+            Node reversedListRoot = null;
+            Node reversedListTail = null;
+
+            Node temp = root;
+            Node reversePoint = temp;
+            while (temp != null && temp.data != start)
+            {
+                reversePoint = temp;
+                temp = temp.next;
+            }
+
+            if (temp == null)
+                return;
+
+            reversedListTail = temp;
+            bool endLoop = false;
+            while (temp != null)
+            {
+                if (reversedListRoot == null)
+                {
+                    reversedListRoot = temp;
+                    temp = temp.next;
+                    reversedListRoot.next = null;
+                }
+                else
+                {
+                    Node previousRevRoot = reversedListRoot;
+                    reversedListRoot = temp;
+                    temp = temp.next;
+                    reversedListRoot.next = previousRevRoot;
+                }
+
+                if (endLoop)
+                    break;
+                endLoop = temp.data == end;
+            }
+
+            if (temp != null)
+                reversedListTail.next = temp;
+            reversePoint.next = reversedListRoot;
+        }
+    }
+}
