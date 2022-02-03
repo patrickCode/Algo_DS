29b65be0f273dbc83feef31980bffb1e721ac125
Pratik Bhattacharya <pratikb@microsoft.com>
2022-02-04 00:03:36 +0530

    algo: find minimum in rotated array

---
+++ GeneralAlgo/GeneralAlgo/RotatedArray.cs
@@ -0,0 +1,37 @@
+﻿namespace GeneralAlgo
+{   
+    public class RotatedArray
+    {
+        private readonly int[] _array;
+
+        public RotatedArray(int[] array)
+        {
+            _array = array;
+        }
+
+        // Problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+        // Reference: https://www.youtube.com/watch?v=nIVW4P8b1VA
+        public int Min()
+        {
+            int low = 0, mid;
+            int high = _array.Length - 1;
+            
+            while (low < high)
+            {
+                mid = (low + high) / 2;
+                if (_array[low] < _array[high])
+                    return _array[low];
+
+                if (_array[mid] >= _array[low])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return _array[low];
+        }
+    }
+}
---
+++ GeneralAlgo/GeneralAlgoTests/RotatedArrayTests.cs
@@ -0,0 +1,36 @@
+﻿using GeneralAlgo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeneralAlgoTests
+{
+    [TestClass]
+    public class RotatedArrayTests
+    {
+        [TestMethod]
+        public void TestCase1_FindMin_Rot2()
+        {
+            int[] array = new int[] { 3, 4, 5, 1, 2 };
+            RotatedArray rArr = new(array);
+            int min = rArr.Min();
+            Assert.AreEqual(1, min);
+        }
+
+        [TestMethod]
+        public void TestCase1_FindMin_Rot0()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            RotatedArray rArr = new(array);
+            int min = rArr.Min();
+            Assert.AreEqual(1, min);
+        }
+
+        [TestMethod]
+        public void TestCase1_FindMin_Rot6()
+        {
+            int[] array = new int[] { 6, 7, 0, 1, 2, 4, 5 };
+            RotatedArray rArr = new(array);
+            int min = rArr.Min();
+            Assert.AreEqual(0, min);
+        }
+    }
+}
