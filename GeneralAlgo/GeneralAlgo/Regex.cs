7c280ca51397706218e76683200b66463308072c
Pratik Bhattacharya <pratikb@microsoft.com>
2022-02-11 11:09:45 +0530

    regex: added caching

--- GeneralAlgo/GeneralAlgo/Regex.cs
+++ GeneralAlgo/GeneralAlgo/Regex.cs
@@ -1,4 +1,6 @@
 ﻿// Reference: https://www.youtube.com/watch?v=HAA8mgxlov8
+using System.Collections.Generic;
+
 namespace GeneralAlgo
 {
     public class Regex
@@ -11,11 +13,15 @@ namespace GeneralAlgo
 
         public bool Match(string word)
         {
-            return Match(word, 0, _pattern, 0);
+            return Match(word, 0, _pattern, 0, new());
         }
 
-        private bool Match(string word, int wordIndex, string pattern, int patternIndex)
+        private bool Match(string word, int wordIndex, string pattern, int patternIndex, Dictionary<string, bool> cache)
         {
+            string indexCacheKey = $"{wordIndex},{patternIndex}";
+            if (cache.ContainsKey(indexCacheKey))
+                return cache[indexCacheKey];
+
             bool isWordInBound = wordIndex < word.Length;
             bool isPatternInBound = patternIndex < pattern.Length;
 
@@ -26,7 +32,7 @@ namespace GeneralAlgo
                 return false;
 
             char patternToMatch = pattern[patternIndex];
-            char wordToMatch = isWordInBound ? word[wordIndex] : ' ';
+            char wordToMatch = isWordInBound ? word[wordIndex] : '\0';
 
             bool isStarPattern = patternIndex < pattern.Length - 1 && pattern[patternIndex + 1] == '*';
             bool isPatternIndexMatching = isWordInBound && (wordToMatch == patternToMatch || patternToMatch == '.');
@@ -35,18 +41,25 @@ namespace GeneralAlgo
             {
                 if (isPatternIndexMatching) 
                 {
-                    return Match(word, wordIndex + 1, pattern, patternIndex + 1);
+                    bool isMatch = Match(word, wordIndex + 1, pattern, patternIndex + 1, cache);
+                    cache.Add(indexCacheKey, isMatch);
+                    return isMatch;
                 }
+                cache.Add(indexCacheKey, false);
                 return false;
             }
 
             if (isPatternIndexMatching)
             {
-                return Match(word, wordIndex + 1, pattern, patternIndex + 2) // Choice 1: Match the star pattern once
-                    || Match(word, wordIndex, pattern, patternIndex + 2) // Choice 2: Don't match the star pattern (0 times)
-                    || Match(word, wordIndex + 1, pattern, patternIndex); // Choice: Match the star pattern multiple times
+                bool isMatch = Match(word, wordIndex + 1, pattern, patternIndex + 2, cache) // Choice 1: Match the star pattern once
+                    || Match(word, wordIndex, pattern, patternIndex + 2, cache) // Choice 2: Don't match the star pattern (0 times)
+                    || Match(word, wordIndex + 1, pattern, patternIndex, cache); // Choice: Match the star pattern multiple times
+                cache.Add(indexCacheKey, isMatch);
+                return isMatch;
             }
-            return Match(word, wordIndex, pattern, patternIndex + 2);
+            bool isNextPattenMacthed = Match(word, wordIndex, pattern, patternIndex + 2, cache);
+            cache.Add(indexCacheKey, isNextPattenMacthed);
+            return isNextPattenMacthed;
         }
     }
 }
